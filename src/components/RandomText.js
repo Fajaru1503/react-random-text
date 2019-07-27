024de76cf2e4e22539fc616f99ca88e1f6cfb80e
fajaru <fajarudewangga@gmail.com>
2019-07-28 01:44:37 +0800

    initialize component random text

---
+++ src/components/RandomText.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+class RandomText extends Component {
+  constructor(props) {
+    super(props);
+    this.names = ["Fajaru", "Fajar", "Dewangga"];
+    this.randomNumber = 0;
+  }
+
+  state = {
+    randomText: "",
+    playStatus: false
+  };
+
+  startRandomHandler = () => {
+    this.setState({ playStatus: true });
+
+    this.interval = setInterval(() => {
+      this.randomNumber = Math.floor(Math.random() * this.names.length);
+      const selectedName = this.names[this.randomNumber];
+      this.setState({ randomText: selectedName });
+    }, 100);
+  };
+
+  stopRandomHandler = () => {
+    clearInterval(this.interval);
+    this.setState({ playStatus: false });
+  };
+
+  componentDidMount() {
+    if (this.state.randomText < 1) {
+      this.setState({ randomText: "Are You Ready?" });
+    }
+  }
+
+  render() {
+    const centerContent = {
+      left: "50%",
+      position: "absolute",
+      top: "50%",
+      transform: "translate(-50%)"
+    };
+
+    return (
+      <div style={centerContent}>
+        <h1>{this.state.randomText}</h1>
+      </div>
+    );
+  }
+}
+
+export default RandomText;
